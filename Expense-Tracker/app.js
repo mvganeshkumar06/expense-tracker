4b9289f666ae793e3a5c25918133909df9580704
Ganesh Kumar <mvganeshk15@gmail.com>
2020-11-09 18:44:07 +0530

    Code refactored and added session storage for the app

--- Expense-Tracker/app.js
+++ Expense-Tracker/app.js
@@ -9,29 +9,6 @@ const deleteIcon = `<img
    src="https://img.icons8.com/flat_round/64/000000/delete-sign.png"
  />`;
 
-let currExpense = 0;
-
-btn.addEventListener("click", () => {
-  const type = expenseType.value;
-  const amt = expenseAmount.value;
-  expenseType.value = "";
-  expenseAmount.value = "";
-  createExpenseCard(type, amt);
-  updateTotalExpense(amt);
-});
-
-container.addEventListener("click", (event) => {
-  const currBtn = event.target;
-  if (currBtn.id === "deleteIcon") {
-    currExpense -= parseInt(
-      currBtn.previousElementSibling.textContent.replace("$", "")
-    );
-    totalExpense.textContent = `Total Expense : ${currExpense}$`;
-    const currDiv = currBtn.parentElement;
-    container.removeChild(currDiv);
-  }
-});
-
 function createExpenseCard(expenseType, amount) {
   const div = document.createElement("div");
   const type = document.createElement("h3");
@@ -51,7 +28,50 @@ function createExpenseCard(expenseType, amount) {
   container.appendChild(div);
 }
 
+let currExpense = 0;
+
 function updateTotalExpense(amount) {
   currExpense += parseInt(amount);
   totalExpense.textContent = `Total Expense : ${currExpense}$`;
 }
+
+let localContainer = [];
+
+btn.addEventListener("click", () => {
+  const type = expenseType.value;
+  const amt = expenseAmount.value;
+
+  localContainer.push({ type, amt });
+
+  localStorage.setItem("localArr", JSON.stringify(localContainer));
+
+  expenseType.value = "";
+  expenseAmount.value = "";
+  createExpenseCard(type, amt);
+  updateTotalExpense(amt);
+});
+
+container.addEventListener("click", (event) => {
+  const currBtn = event.target;
+  if (currBtn.id === "deleteIcon") {
+    currExpense -= parseInt(
+      currBtn.previousElementSibling.textContent.replace("$", "")
+    );
+    totalExpense.textContent = `Total Expense : ${currExpense}$`;
+    const currDiv = currBtn.parentElement;
+    container.removeChild(currDiv);
+  }
+});
+
+window.addEventListener("load", () => {
+  let localArr = JSON.parse(localStorage.getItem("localArr"));
+
+  console.log("wait the data is coming...");
+
+  if (localArr) {
+    localArr.map((item) => {
+      createExpenseCard(item.type, item.amt);
+      updateTotalExpense(item.amt);
+    });
+  }
+});
